968e7f7a946bff34eb54716338741b83b0804e11
wlt <wanglitao1202@outlook.com>
2015-06-11 15:44:32 +0800

    Use the chain programming implementation choose_multiples_of_three

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-    var _= require('/home/wlt/loadsh/myloadsh.js');
-    return _.filter(collection,function(n){
-        return n%3===0;
-    });
+    var _ = require("../../lodash.js");
+    return _(collection).filter(function(n){
+
+    },function(a,b){
+        return a%3 === 0;
+    }).value();
 }
 
 module.exports = choose_multiples_of_three;
