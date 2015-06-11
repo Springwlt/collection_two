2dede28c73a2598e54f78ed530681b9db603a26c
wlt <wanglitao1202@outlook.com>
2015-06-11 16:02:43 +0800

    Use the chain programming implementation collect_min_number

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function collect_min_number(collection) {
-    var _= require('/home/wlt/loadsh/myloadsh.js');
-    return _.min(collection);
+    var _= require('../../lodash.js');
+    return _(collection).min(collection).value();
 }
 module.exports = collect_min_number;
