ab1354d9bdf56507ac756fb3a8faffd26076cbc0
wlt <wanglitao1202@outlook.com>
2015-06-11 14:50:27 +0800

    使用链式编程实现choose_even

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,10 +1,11 @@
 'use strict';
 
 function choose_even(collection) {
-    var _= require('/home/wlt/loadsh/myloadsh.js');
-    return _.filter(collection,function(n){
-        return n%2===0;
-    });
-}
+    var _=require("../../lodash.js");
+    return _(collection).filter(function(n){
 
+    },function(a,b){
+        return a%2 === 0;
+    }).value();
+}
 module.exports = choose_even;
