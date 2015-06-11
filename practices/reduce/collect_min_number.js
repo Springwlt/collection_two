9aaa4b777c97a6dabb212ac5d2cfcfc19577957b
wlt <wanglitao1202@outlook.com>
2015-06-11 16:48:49 +0800

    Use the chain programming implementation collect_min_number

--- practices/reduce/collect_min_number.js
+++ practices/reduce/collect_min_number.js
@@ -2,6 +2,6 @@
 
 function collect_min_number(collection) {
     var _= require('../../lodash.js');
-    return _(collection).min(collection).value();
+    return _(collection).min().value();
 }
 module.exports = collect_min_number;
