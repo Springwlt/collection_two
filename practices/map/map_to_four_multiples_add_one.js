f72508e176a0287bbfdce71f64ec14f6f39026e5
wlt <wanglitao1202@outlook.com>
2015-06-11 14:27:52 +0800

    Use the chain programminging implementation map_to_fore_mutiples_add_one

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,8 +1,8 @@
 'use strict';
 function map_to_four_multiples_add_one(collection){
-    var _= require('/home/wlt/loadsh/myloadsh.js');
-    return _.map(collection,function(n){
+    var _= require("../../lodash.js");
+    return _(collection).map(function(n){
         return n*4+1;
-    });
+    }).value();
 }
 module.exports = map_to_four_multiples_add_one;
