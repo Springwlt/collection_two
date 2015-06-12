08df2088e47793e627562ac61c02466e546cba91
wlt <wanglitao1202@outlook.com>
2015-06-12 11:27:52 +0800

    Using the protopyte chain collect_all_even

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,10 +1,10 @@
 'use strict';
 
 function collect_all_even(collection) {
-    var _= require('/home/wlt/loadsh/myloadsh.js');
-    return _.filter(collection,function(n){
-        return n%2 === 0 ;
-    })
+    var _= require('../../lodash.js');
+    return _(collection).filter(function(n,i){
+        return n%2 === 0;
+    }).value();
 }
 
 module.exports = collect_all_even;
