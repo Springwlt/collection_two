c9b4751e28f0870dd4f23ed7f5cdb54fb105aab4
wlt <wanglitao1202@outlook.com>
2015-06-12 13:29:45 +0800

    Usering the prototype chain get_integer_interval_2

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,12 +1,12 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-    var _= require("/home/wlt/loadsh/myloadsh.js");
-    var median_result =_.range(number_a,number_b);
-    var result =_.filter(median_result,function(n,i){
+    var _= require("../../lodash.js");
+    var even_result = _().range(number_a,number_b).value();
+    //console.log(even_result);
+    return _(even_result).filter(function(n,i){
         return n%2 === 0;
-    });
-    return result;
+    }).value();
 }
 
 module.exports = get_integer_interval_2;
