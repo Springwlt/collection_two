76719da59725f9e28331ccdc2b8ec4aaafefe28f
wlt <wanglitao1202@outlook.com>
2015-06-14 21:02:30 +0800

    Using the prototype chain get_union.js

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -1,11 +1,13 @@
 'use strict';
 
 function get_union(collection_a, collection_b) {
-    var _=require("/home/wlt/loadsh/myloadsh.js");
-    for(var i = 0; i <collection_b.length;i++) {
+    var _ = require('../../lodash.js');
+
+    for(var i = 0; i < collection_b.length; i++) {
         collection_a.push(collection_b[i]);
     }
-    return _.no_repeat(collection_a);
+    return _(collection_a).no_repeat().value();
+    
 }
 
 module.exports = get_union;
