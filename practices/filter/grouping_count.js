b575db1faa8e331c75713e2ac6a03838a087d0a3
wlt <wanglitao1202@outlook.com>
2015-06-14 21:14:59 +0800

    Using the prototype chain grouping_count.js

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,14 +1,16 @@
 'tuse strict';
 
 function grouping_count(collection) {
-    var _= require("/home/wlt/loadsh/myloadsh.js");
+    var _ = require("../../lodash.js");
     var  result = {};
-    
-    _.foreach(collection,function(n,i){
+
+    _(collection).each(function(n,i){
         result[[n]] = result[[n]] || 0;
         result[n]++;
     });
+
     return result;
+    
 }
 
 module.exports = grouping_count;
