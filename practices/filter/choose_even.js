6fe2518779726e7300c838af6b94b94df377ae76
wlt <wanglitao1202@outlook.com>
2015-06-14 21:11:44 +0800

    Using the prototype chain choose_even.js

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function choose_even(collection) {
-    var _=require("../../lodash.js");
+    var _ = require("../../lodash.js");
+
     return _(collection).filter(
     function(a,b){
         return a%2 === 0;
     }).value();
+
 }
 module.exports = choose_even;
