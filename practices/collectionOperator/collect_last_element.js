a8d74dabacf49627d23fee20a1d22b178dd64539
wlt <wanglitao1202@outlook.com>
2015-06-14 20:57:17 +0800

    Using the prototype chain collec_last_elemet.js

--- practices/collectionOperator/collect_last_element.js
+++ practices/collectionOperator/collect_last_element.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function collect_last_element(collection) {
-    var _= require('../../lodash.js');
+    var _ = require('../../lodash.js');
+
     return _(collection).reduce(function(a,b){
         return b;
     }).value();
+    
 }
 module.exports = collect_last_element;
