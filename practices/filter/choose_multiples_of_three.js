7e6019ba1dbd62acb80869f08cebca01ed86a921
wlt <wanglitao1202@outlook.com>
2015-06-14 21:12:56 +0800

    Using the prototype chain choose_multiples_number.js

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -2,10 +2,12 @@
 
 function choose_multiples_of_three(collection) {
     var _ = require("../../lodash.js");
+
     return _(collection).filter(
     function(a,b){
         return a%3 === 0;
     }).value();
+    
 }
 
 module.exports = choose_multiples_of_three;
