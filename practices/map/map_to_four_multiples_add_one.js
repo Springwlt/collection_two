ae5c08a0f89b02204421227d0a8f15365afedcb6
wlt <wanglitao1202@outlook.com>
2015-06-14 21:22:16 +0800

    Using the prototype chain map_to_fore_multiples_add_one.js

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,8 +1,10 @@
 'use strict';
 function map_to_four_multiples_add_one(collection){
-    var _= require("../../lodash.js");
+    var _ = require("../../lodash.js");
+    
     return _(collection).map(function(n){
         return n*4+1;
     }).value();
+
 }
 module.exports = map_to_four_multiples_add_one;
