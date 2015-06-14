055060ec913cc19e54be7113fa3e9bd958088290
wlt <wanglitao1202@outlook.com>
2015-06-14 20:59:13 +0800

    Using the prototype chain get_integer_interval_2.js

--- practices/collectionOperator/get_integer_interval_2.js
+++ practices/collectionOperator/get_integer_interval_2.js
@@ -1,12 +1,14 @@
 'use strict';
 
 function get_integer_interval_2(number_a, number_b) {
-    var _= require("../../lodash.js");
+    var _ = require("../../lodash.js");
+
     var even_result = _().range(number_a,number_b).value();
-    //console.log(even_result);
+
     return _(even_result).filter(function(n,i){
         return n%2 === 0;
     }).value();
+
 }
 
 module.exports = get_integer_interval_2;
