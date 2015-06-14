15bcd6c7923c4f37022ad664988e30da00359724
wlt <wanglitao1202@outlook.com>
2015-06-14 21:25:41 +0800

    Using the prototype chain rank_asc.js

--- practices/map/rank_asc.js
+++ practices/map/rank_asc.js
@@ -1,9 +1,11 @@
 'use strict';
 var rank_asc = function(collection){
-    var _=require("/home/wlt/loadsh/myloadsh.js");
-    return _.sort(collection,function(a,b){
-        return a < b;
-    });
+    var _=require("../../lodash.js");
+
+    return _(collection).sort(function(a,b){
+        return a > b;
+    }).value();
+    
 };
 
 module.exports = rank_asc;
