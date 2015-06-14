dab1dcda79fb3a35b8e5e8601e77feb65be60800
wlt <wanglitao1202@outlook.com>
2015-06-14 21:32:23 +0800

    Using the prototype chain compare_collection.js

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -1,17 +1,19 @@
 'use strict';
 
 function compare_collections(collection_a, collection_b) {
-    var _= require("/home/wlt/loadsh/myloadsh.js");
+    var _ = require("../../lodash.js");
     var mark = false;
 
-     _.foreach(collection_b,function(n){
-        _.foreach(collection_a,function(a){
+     _(collection_b).each(function(n){
+        _(collection_a).each(function(a){
             if(n === a) {
                 mark = true;
             }
         });
     });
+
     return mark;
+
 }
 
 module.exports = compare_collections;
