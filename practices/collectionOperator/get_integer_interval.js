630e01ea567a2eaf111215fe53d66ee25367f26d
wlt <wanglitao1202@outlook.com>
2015-06-14 20:58:24 +0800

    Using the prototype chain get_integer_interval.js

--- practices/collectionOperator/get_integer_interval.js
+++ practices/collectionOperator/get_integer_interval.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-    var _= require("../../lodash.js");
+    var _ = require("../../lodash.js");
+
     return _().range(number_a,number_b).value();
+    
 }
 
 module.exports = get_integer_interval;
