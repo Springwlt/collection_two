47980b9be327e9eebb3627bf52ebe1ecc574680a
wlt <wanglitao1202@outlook.com>
2015-06-14 21:21:28 +0800

    Using the prototype chain map_to_even.js

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,9 +1,11 @@
 'use strict';
 function map_to_even(collections){
-    var _= require('../../lodash.js');
+    var _ = require('../../lodash.js');
+
     return _(collections).map(function(n){
         return n*2;
     }).value();
+
 }
 
 module.exports = map_to_even;
