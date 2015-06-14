102e5c54f2dfc8f314aea2554807618fff6b0314
wlt <wanglitao1202@outlook.com>
2015-06-14 21:23:06 +0800

    Using the prototype chain map_to_there_multiples.js

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,7 +1,8 @@
 'use strict';
 
  function map_to_three_multiples(collection){
-    var _= require("../../lodash");
+    var _ = require("../../lodash");
+
     return  _(collection).map(function(n){
         return n*3;
     }).value();
