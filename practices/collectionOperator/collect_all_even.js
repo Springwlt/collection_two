26f436817c13de2a64fd5985f273388feb4d0826
wlt <wanglitao1202@outlook.com>
2015-06-14 20:56:11 +0800

    Using the prototype chain collec_all_even.js

--- practices/collectionOperator/collect_all_even.js
+++ practices/collectionOperator/collect_all_even.js
@@ -1,10 +1,12 @@
 'use strict';
 
 function collect_all_even(collection) {
-    var _= require('../../lodash.js');
+    var _ = require('../../lodash.js');
+
     return _(collection).filter(function(n,i){
         return n%2 === 0;
     }).value();
+
 }
 
 module.exports = collect_all_even;
