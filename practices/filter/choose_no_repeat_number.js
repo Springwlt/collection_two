116962a29020c7025b2621997fe9e1484c351ba1
wlt <wanglitao1202@outlook.com>
2015-06-14 21:13:44 +0800

    Using the prototype chain choose_no_repeat_number.js

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-    var _=require("/home/wlt/loadsh/myloadsh.js");
-    return _.no_repeat(collection);
+    var _ = require("../../lodash.js");
+
+    return _(collection).no_repeat().value();
+
 }
 module.exports = choose_no_repeat_number;
