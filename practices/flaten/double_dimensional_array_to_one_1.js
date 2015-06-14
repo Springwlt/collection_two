766c482eb3bd6fb60cafe1f7a88f21159cbacab7
wlt <wanglitao1202@outlook.com>
2015-06-14 21:18:36 +0800

    Using the prototype chain double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function double_to_one(collection) {
-    var _= require("/home/wlt/loadsh/myloadsh.js");
-    return _.two_chang_one(collection);
+    var _= require("../../lodash.js");
+    return _().two_chang_one(collection).value();
 }
 module.exports = double_to_one;
