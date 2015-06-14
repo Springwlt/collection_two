66c60fdac7dd788c157676f18d1f6f6ed28d9229
wlt <wanglitao1202@outlook.com>
2015-06-14 21:37:12 +0800

    Using the prototype chain find_first_even.js

--- practices/reduce/find_first_even.js
+++ practices/reduce/find_first_even.js
@@ -1,12 +1,14 @@
 'use strict';
 function find_first_even(collection) {
-    var _= require('../../lodash.js');
+    var _ = require('../../lodash.js');
+
     var result= _(collection).filter(
         function(n,i){
             return n%2 === 0;
     }).value();
-    console.log(result);
+
     return _(result).first().value();
+
 }
 
 module.exports = find_first_even;
