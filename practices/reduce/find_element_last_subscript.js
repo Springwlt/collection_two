b4c4a8e87ab97644facb2a5c0d378eb6bc8889b1
wlt <wanglitao1202@outlook.com>
2015-06-14 21:36:42 +0800

    Using the prototype chain find_element_last_subscript.js

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -1,13 +1,15 @@
 'use strict';
 
 function calculate_elements_sum(collection, element) {
-    var _= require("/home/wlt/loadsh/myloadsh.js");
+    var _= require("../../lodash.js");
     var index = -1;
-    _.foreach(collection,function(n,i){
+
+    _(collection).each(function(n,i){
         if(n === element) {
             index = i;
         }
     });
+    
     return index;
 }
 
