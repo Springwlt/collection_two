44f0674d11b6b7088ec1b704aadaf455cb392279
wlt <wanglitao1202@outlook.com>
2015-06-14 21:16:41 +0800

    Using the prototype chain two_collections_practice_2.js

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -1,20 +1,23 @@
 'use strict';
 
 function choose_no_common_elements(collection_a, collection_b) {
-    var _= require("/home/wlt/loadsh/myloadsh.js");
+    var _ = require("../../lodash.js");
 
-    for(var i = 0; i <collection_b.length;i++) {
+    for(var i = 0; i < collection_b.length; i++) {
         collection_a.push(collection_b[i]);
     }
-    var result = _.count(collection_a);
+
+    var result = _(collection_a).count();
     var array = [];
-    
+
     for(var key in result) {
         if(result[key]===1) {
             array[array.length] = key;
         }
     }
+
     return array;
+    
 }
 
 module.exports = choose_no_common_elements;
