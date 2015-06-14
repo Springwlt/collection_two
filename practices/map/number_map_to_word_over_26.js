0bfcc5a37e0b791f98270dac5a886204e9169317
wlt <wanglitao1202@outlook.com>
2015-06-14 21:24:59 +0800

    Using the prototype chain nkumber_map_to_word_over_26.js

--- practices/map/number_map_to_word_over_26.js
+++ practices/map/number_map_to_word_over_26.js
@@ -1,11 +1,13 @@
 'use strict';
 var number_map_to_word_over_26 = function(collection){
-    var _= require("/home/wlt/loadsh/myloadsh.js");
+    var _ = require("../../lodash.js");
     var array = [];
+
     for(var i = 0; i < collection.length; i++) {
-        array.push(_.letter(collection[i]));
+        array.push(_().letter(collection[i]));
     }
     return array;
+    
 };
 
 module.exports = number_map_to_word_over_26;
