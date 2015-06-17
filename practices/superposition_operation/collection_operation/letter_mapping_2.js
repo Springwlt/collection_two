0627b6816355f0e72a94c83a9d27cbac43b502b0
wlt <wanglitao1202@outlook.com>
2015-06-17 16:50:49 +0800

    Using the prototype chain letter_mapping_2.js

--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,18 +1,12 @@
 'use strict';
 
 function average_to_letter(collection) {
-    // var sum = 0;
-    // var average = 0;
-    // var index = 0;
-    // for(var i = 0;i < collection.length;i++) {
-    //     sum += collection[i];
-    // }
-    // average = Math.ceil(sum/collection.length);
-    // index = String.fromCharCode(average+96);
-    // return index;
-    var _= require("/home/wlt/loadsh/myloadsh.js")
-    var average =Math.ceil(_.sum(collection)/collection.length);
-    return String.fromCharCode(average+96);
+    var _= require("../../../lodash.js")
+    var sum =(_(collection).sum().value());
+
+    var average =Math.ceil(sum/collection.length);
+    return _().letter(average);
+    
 }
 
 module.exports = average_to_letter;
