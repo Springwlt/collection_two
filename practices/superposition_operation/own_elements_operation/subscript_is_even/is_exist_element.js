60a1b5626bcd53dfcbb1cad3da1119fc469e7082
wlt <wanglitao1202@outlook.com>
2015-06-17 17:15:03 +0800

    Using the prototype chain is_exist_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,10 +1,13 @@
 'use strict'
 var is_exist_element = function(collection,element){
-    var _= require('/home/wlt/loadsh/myloadsh.js');
-    var result = _.filter(collection,function(n,i){
+    var _= require('../../../../lodash.js');
+
+    var result = _(collection).filter(function(n,i){
         return i%2 === 0;
-    });
-    return  _.exist(result,element);
+    }).value();
+
+    return  _(result).exist(element).value();
+    
 };
 
 module.exports = is_exist_element;
