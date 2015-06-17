69676aec25016acf6c12e10e790db85a16f74e89
wlt <wanglitao1202@outlook.com>
2015-06-17 17:21:29 +0800

    Using the prototype chain one_add_next_multiply_three.js

--- practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
+++ practices/superposition_operation/own_elements_operation/one_add_next_multiply_three.js
@@ -1,15 +1,15 @@
 'use strict';
 function one_add_next_multiply_three(collection){
-    // var result = [];
-    //
-    // for(var i = 0; i < collection.length-1; i++) {
-    //     result.push((collection[i]+collection[i+1])*3);
-    // }
-    // return result;
-    var _=require("/home/wlt/loadsh/myloadsh.js");
-    var result = _.foreach(collection,function(n,i){
-         n = n+ (n+1);
-    })
-    console.log(result);
-}
-module.exports = one_add_next_multiply_three;
+    var _ = require("../../../lodash.js");
+    var result = [];
+
+    _(collection).each(function(n,i){
+        if(i  < collection.length-1) {
+            result.push((n + collection[i+1])*3);
+        }
+    });
+
+    return result;
+    
+ }
+ module.exports = one_add_next_multiply_three;
