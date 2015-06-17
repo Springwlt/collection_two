54d4649b3b5d84aabe03ff3a10628a8caf6e1353
wlt <wanglitao1202@outlook.com>
2015-06-17 16:53:10 +0800

    Using the prototype chain practice_3.js

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,22 +1,20 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
-    // var sum = 0;
-    //
-    // for(var i = 0; i < collection.length;i++) {
-    //     if(collection[i]%2 != 0 ) {
-    //         sum += collection[i]*3+5;
-    //     }
-    // }
-    // return sum;
-    var _=require("/home/wlt/loadsh/myloadsh.js");
-    var result =_.filter(collection,function(n,i){
-        return n%2 != 0;
-    });
-    var array=_.map(result,function(n){
-        return n*3+5;
-    });
-    return _.sum(array);
-}
+    var _=require("../../../lodash.js");
 
+    var result =_(collection).filter(function(n,i){
+        return n%2 != 0;
+    }).value();
+
+    var array=_(result).map(function(n){
+        return n*3+5;
+    }).value();
+
+    var sum =  _(array).reduce(function(a,b){
+        return a+b;
+    }).value();
+    
+    return sum;
+}
 module.exports = hybrid_operation_to_uneven;
