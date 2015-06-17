718b4790272c7189e09d32237b33514ec8f0afcd
wlt <wanglitao1202@outlook.com>
2015-06-17 17:20:05 +0800

    Using the prototype chain even_asc_odd_desc.js

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,21 +1,27 @@
 'use strict';
 var even_asc_odd_desc = function(collection){
-    var _= require("/home/wlt/loadsh/myloadsh.js");
-    var even_result = _.filter(collection,function(n,i){
+    var _= require("../../../lodash.js");
+
+    var even_array = _(collection).filter(function(n,i){
         return n%2 === 0;
-    });
-    var odd_result = _.filter(collection,function(n,i){
+    }).value();
+
+    var odd_array = _(collection).filter(function(n,i){
         return n%2 != 0;
-    });
-    _.sort(even_result,function(a,b){
-        return a > b;
-    });
-    _.sort(odd_result,function(a,b){
-        return b > a;
-    });
-    _.foreach(odd_result,function(n,i){
+    }).value();
+
+    var even_result = _(even_array).sort(function(a,b){
+        return a < b;
+    }).value();
+
+    var odd_result = _(odd_array).sort(function(a,b){
+        return b < a;
+    }).value();
+
+    _(odd_result).each(function(n,i){
         even_result.push(n);
-    })
+    });
+    
      return even_result;
 }
 module.exports = even_asc_odd_desc;
