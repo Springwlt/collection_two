663e6c980d78b9dd375595c380fb42e1763cfa7b
wlt <wanglitao1202@outlook.com>
2015-06-17 17:16:20 +0800

    Using the prototype chain single_element.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,25 +1,26 @@
 'use strict';
 var single_element = function(collection) {
-    var _ =require("/home/wlt/loadsh/myloadsh.js");
+    var _ =require("../../../../lodash.js");
 
-    var even_result = _.filter(collection,function(n,i){
+    var even_result = _(collection).filter(function(n,i){
         return i%2 != 0;
-    });
+    }).value();
 
     var temp = {};
-    _.foreach(even_result,function(n,i){
+    _(even_result).each(function(n,i){
             temp[n+" "] = temp[n+" "]||0;
             temp[n+" "]++;
     });
 
     var result = [];
+
     for(var key in temp) {
         if(temp[key] === 1) {
             result.push(parseInt(key));
         }
     }
     return result;
-    var result= _.no_repeat(even_result);
+    
 }
 
 module.exports = single_element;
