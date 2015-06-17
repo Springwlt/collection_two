4b105bdb0ddfa5b400a4cc74c2c91af180a54560
wlt <wanglitao1202@outlook.com>
2015-06-17 17:13:55 +0800

    Using the prototype chain even_group_calculate_average.js

--- practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/even_group_calculate_average.js
@@ -1,23 +1,28 @@
 'use strict';
 var even_group_calculate_average = function(collection){
-	var _= require("/home/wlt/loadsh/myloadsh.js");
+	var _= require("../../../../lodash.js");
 	var result = [];
-	var even_result=_.filter(collection,function(n,i){
+
+	var even_result=_(collection).filter(function(n,i){
         return n%2 === 0 && (i+1)%2 === 0;
-	});
+	}).value();
+
 	var temp = {};
-	_.foreach(even_result,function(n,i){
+	_(even_result).each(function(n,i){
 		temp[(n+"").length] = temp[(n+"").length] || [];
 		temp[(n+"").length].push(n);
 	});
+
 	var sum = 0;
 	for(var i in temp) {
 		var arr = temp[i];
-        sum = _.reduce(arr,function(a,b){
+        sum = _(arr).reduce(function(a,b){
 			return a+b;
-		});
+		}).value();
 		result.push(sum/arr.length);
 	}
+
 	return result.length=== 0 ? [0]:result;
+
 };
 module.exports = even_group_calculate_average;
